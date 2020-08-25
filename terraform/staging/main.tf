98a1aa8d027ce9b33720f9639e589ce0b08cfac8
roy <royhanley8@gmail.com>
2020-08-25 16:32:36 +0100

    Added back Django + s3

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -188,40 +188,40 @@ resource "aws_security_group" "rabbit_management" {
 ###############################################################################
 # Django instance
 ###############################################################################
-//resource "aws_instance" "django" {
-//  ami                    = "ami-0917237b4e71c5759"
-//  instance_type          = "t2.micro"
-//  key_name               = aws_key_pair.deployer.key_name
-//  tags                   = {
-//    Name = "${var.environment}_fn_django",
-//  }
-//  vpc_security_group_ids = [
-//    aws_security_group.http.id,
-//    aws_security_group.https.id,
-//    aws_security_group.rabbit.id,
-//    aws_security_group.ssh.id,
-//  ]
-//}
-//
-//resource "aws_eip" "django" {
-//    vpc      = true
-//    instance = aws_instance.django.id
-//}
-//
-//output "django_server_public_ip" {
-//  value = aws_eip.django.public_ip
-//}
-//
-//output "django_server_private_ip" {
-//  value = aws_instance.django.private_ip
-//}
-//
-//resource "cloudflare_record" "django" {
-//  zone_id = var.cloudflare_zone_id
-//  name    = var.django_name
-//  value   = aws_eip.django.public_ip
-//  type    = "A"
-//}
+resource "aws_instance" "django" {
+  ami                    = "ami-0917237b4e71c5759"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.deployer.key_name
+  tags                   = {
+    Name = "${var.environment}_fn_django",
+  }
+  vpc_security_group_ids = [
+    aws_security_group.http.id,
+    aws_security_group.https.id,
+    aws_security_group.rabbit.id,
+    aws_security_group.ssh.id,
+  ]
+}
+
+resource "aws_eip" "django" {
+    vpc      = true
+    instance = aws_instance.django.id
+}
+
+output "django_server_public_ip" {
+  value = aws_eip.django.public_ip
+}
+
+output "django_server_private_ip" {
+  value = aws_instance.django.private_ip
+}
+
+resource "cloudflare_record" "django" {
+  zone_id = var.cloudflare_zone_id
+  name    = var.django_name
+  value   = aws_eip.django.public_ip
+  type    = "A"
+}
 
 ###############################################################################
 # Database instance
@@ -340,47 +340,47 @@ output "scoring_worker_server_private_ip" {
 ###############################################################################
 # S3
 ###############################################################################
-//resource "aws_s3_bucket" "media" {
-//  bucket = "${var.environment}-fixthenews"
-//
-//  cors_rule {
-//    allowed_headers = ["*"]
-//    allowed_methods = ["GET", "PUT", "DELETE"]
-//    allowed_origins = [
-//        "https://${var.django_name}.${var.domain_name}",
-//    ]
-//    max_age_seconds = 3000
-//  }
-//}
-//
-//resource "aws_s3_bucket_policy" "media" {
-//  bucket = aws_s3_bucket.media.id
-//  policy = <<POLICY
-//{
-//    "Version": "2012-10-17",
-//    "Id": "${var.environment}_s3_bucket_policy",
-//    "Statement": [
-//        {
-//          "Sid":"Allow get requests originating from ${var.webapp_name}.${var.domain_name}",
-//          "Effect":"Allow",
-//          "Principal":"*",
-//          "Action":["s3:GetObject"],
-//          "Resource":"${aws_s3_bucket.media.arn}/*",
-//          "Condition":{
-//            "StringLike":{"aws:Referer":["https://${var.webapp_name}.${var.domain_name}/*"]}
-//          }
-//        },
-//        {
-//          "Sid":"Allow get, put and delete requests originating from ${var.django_name}.${var.domain_name}",
-//          "Effect":"Allow",
-//          "Principal":"*",
-//          "Action":["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
-//          "Resource":"${aws_s3_bucket.media.arn}/*",
-//          "Condition":{
-//            "StringLike":{"aws:Referer":["https://${var.django_name}.${var.domain_name}/*"]}
-//          }
-//        }
-//    ]
-//}
-//POLICY
-//}
+resource "aws_s3_bucket" "media" {
+  bucket = "${var.environment}-fixthenews"
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "PUT", "DELETE"]
+    allowed_origins = [
+        "https://${var.django_name}.${var.domain_name}",
+    ]
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_s3_bucket_policy" "media" {
+  bucket = aws_s3_bucket.media.id
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Id": "${var.environment}_s3_bucket_policy",
+    "Statement": [
+        {
+          "Sid":"Allow get requests originating from ${var.webapp_name}.${var.domain_name}",
+          "Effect":"Allow",
+          "Principal":"*",
+          "Action":["s3:GetObject"],
+          "Resource":"${aws_s3_bucket.media.arn}/*",
+          "Condition":{
+            "StringLike":{"aws:Referer":["https://${var.webapp_name}.${var.domain_name}/*"]}
+          }
+        },
+        {
+          "Sid":"Allow get, put and delete requests originating from ${var.django_name}.${var.domain_name}",
+          "Effect":"Allow",
+          "Principal":"*",
+          "Action":["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
+          "Resource":"${aws_s3_bucket.media.arn}/*",
+          "Condition":{
+            "StringLike":{"aws:Referer":["https://${var.django_name}.${var.domain_name}/*"]}
+          }
+        }
+    ]
+}
+POLICY
+}
