f8326a322c665ed8bcb72dbedec0bd0806597f69
roy <royhanley8@gmail.com>
2020-08-25 16:26:28 +0100

    Remove Django server from terraform

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -188,41 +188,40 @@ resource "aws_security_group" "rabbit_management" {
 ###############################################################################
 # Django instance
 ###############################################################################
-resource "aws_instance" "django" {
-  ami                    = "ami-0917237b4e71c5759"
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.deployer.key_name
-  tags                   = {
-    Name = "${var.environment}_fn_django",
-  }
-  vpc_security_group_ids = [
-    aws_security_group.http.id,
-    aws_security_group.https.id,
-    aws_security_group.rabbit.id,
-    aws_security_group.rabbit_management.id,
-    aws_security_group.ssh.id,
-  ]
-}
-
-resource "aws_eip" "django" {
-    vpc      = true
-    instance = aws_instance.django.id
-}
-
-output "django_server_public_ip" {
-  value = aws_eip.django.public_ip
-}
-
-output "django_server_private_ip" {
-  value = aws_instance.django.private_ip
-}
-
-resource "cloudflare_record" "django" {
-  zone_id = var.cloudflare_zone_id
-  name    = var.django_name
-  value   = aws_eip.django.public_ip
-  type    = "A"
-}
+//resource "aws_instance" "django" {
+//  ami                    = "ami-0917237b4e71c5759"
+//  instance_type          = "t2.micro"
+//  key_name               = aws_key_pair.deployer.key_name
+//  tags                   = {
+//    Name = "${var.environment}_fn_django",
+//  }
+//  vpc_security_group_ids = [
+//    aws_security_group.http.id,
+//    aws_security_group.https.id,
+//    aws_security_group.rabbit.id,
+//    aws_security_group.ssh.id,
+//  ]
+//}
+//
+//resource "aws_eip" "django" {
+//    vpc      = true
+//    instance = aws_instance.django.id
+//}
+//
+//output "django_server_public_ip" {
+//  value = aws_eip.django.public_ip
+//}
+//
+//output "django_server_private_ip" {
+//  value = aws_instance.django.private_ip
+//}
+//
+//resource "cloudflare_record" "django" {
+//  zone_id = var.cloudflare_zone_id
+//  name    = var.django_name
+//  value   = aws_eip.django.public_ip
+//  type    = "A"
+//}
 
 ###############################################################################
 # Database instance
